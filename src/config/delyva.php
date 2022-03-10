d8ca9818cb3bced82161c4eff52a297c78090459
Firdaus Ishaddin <firdausishaddin@gmail.com>
2022-03-11 02:35:46 +0800

    update config file name

--- src/config/delyva.php
+++ src/config/delyva.php
@@ -1,34 +1,34 @@
 <?php
 
 return [
-    
+
     /**
      * DelyvaX Access Token
      */
     'delyva_access_token' => env('DELYVA_ACCESS_TOKEN', ''),
-    
+
     /**
      * DelyvaX Company Id
      */
-    'delyva_company_id' => env('DELYVA_COMPANY_ID', ''), 
+    'delyva_company_id' => env('DELYVA_COMPANY_ID', ''),
 
     /**
      * DelyvaX Customer ID
      */
-    'delyva_customer_id' => env('DELYVA_CUSTOMER_ID', ''), 
+    'delyva_customer_id' => env('DELYVA_CUSTOMER_ID', ''),
 
     /**
      * DelyvaX Endpoint
      */
-    'delyva_ednpoint' => env('DELYVA_EDNPOINT', 'https://api.delyva.app/v1.0'),
+    'delyva_endpoint' => env('DELYVA_ENDPOINT', 'https://api.delyva.app/v1.0'),
 
     /**
      * DelyvaX CDN Endpoint
      */
-    'delyva_cdn_ednpoint' => env('DELYVA_CDN_EDNPOINT', 'https://cdn.delyva.app'), 
+    'delyva_cdn_endpoint' => env('DELYVA_CDN_ENDPOINT', 'https://cdn.delyva.app'),
 
     /**
      * DelyvaX Company Code
      */
-    'delyva_company_code' => env('DELYVA_COMPANY_CODE', 'my'), 
+    'delyva_company_code' => env('DELYVA_COMPANY_CODE', 'my'),
 ];
